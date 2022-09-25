8f75fc506586655a7f1879e857306a679486caf3
bnjmnrsh <orionrush@gmail.com>
2022-09-25 14:54:05 +0100

    fixed bug were scroll to top would trigger a route change

--- src/js/_simple_router.js
+++ src/js/_simple_router.js
@@ -5,27 +5,24 @@ import { fetchArticles } from './_fetch_articles'
  *
  */
 export const simpleRouter = function () {
-  const urlHash = window.location.hash.replace('#', '')
+  let urlHash = window.location.hash.replace('#', '')
   let loadedSection = document
     .querySelector('#app')
     .getAttribute('data-section')
 
-  if (!loadedSection && !urlHash) {
-    // first load on home page
-    loadedSection = 'home'
-    window.location.hash = loadedSection
-    document.querySelector('#app').setAttribute('data-section', 'home')
-  } else if (!loadedSection && urlHash) {
-    // first load, but to a section
+  urlHash = urlHash || 'home' // If there is no hash, were're home.
+
+  if (!loadedSection && urlHash) {
+    // first load, but to a section not the homepage
     loadedSection = urlHash
     document.querySelector('#app').setAttribute('data-section', urlHash)
   }
-  // If we are scrollToTop, then this prevents triggering fetchArticles again
+  // scrollToTop triggers a hashchange, this prevents mutiple fireing of fetchArticles
   if (urlHash === 'null' || urlHash === 'head') {
     window.removeEventListener('hashchange', simpleRouter)
     window.location.hash = loadedSection
-
-    // just enough of a delay to prevent hashchange from doubel firing
+    // delay re-enableing the listener to prevent hashchange firing mutiple times.
+    // Could use a debounce but this works fine.
     setTimeout(() => window.addEventListener('hashchange', simpleRouter), 1)
   } else {
     fetchArticles(urlHash)
