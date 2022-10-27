f2963cb4cee727794be1a676b325abee52f23f98
bnjmnrsh <orionrush@gmail.com>
2022-10-28 00:11:04 +0100

    update import method names

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,10 @@ import { scrollToTop } from './_scroll_to_top.js'
 import { aSections } from './_section_array.js'
 import { simpleRouter } from './_simple_router.js'
 import { buildNav } from './_build_nav.js'
-import { network } from './_network.js'
+import { networkAvaliabilty } from './_network.js'
 const app = document.querySelector('#app')
 
-network()
+networkAvaliabilty('', true)
 buildNav(app, aSections)
 simpleRouter() // kicks off _fetchArticles.js
 scrollToTop('#newsfeed-wrap')
