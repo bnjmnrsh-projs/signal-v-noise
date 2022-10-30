6f85f64864cd2e663ad0b52b3c3d63e808aa190f
bnjmnrsh <orionrush@gmail.com>
2022-10-30 19:04:41 +0000

    Use Satchel to set get cached data

--- src/js/_fetch_articles.js
+++ src/js/_fetch_articles.js
@@ -1,4 +1,5 @@
 import { buildArticles } from './_build_articles'
+import { Satchel } from '@bnjmnrsh/satchel'
 const API = 'https://signal-v-noise-worker.bnjmnrsh.workers.dev'
 
 /**
@@ -37,45 +38,69 @@ ${
 </div>`
 }
 
+function getStoredArticles(section, forced = false) {
+  const storedArticles = Satchel.getSatchel(section, true, 'svn-store')
+  if (storedArticles?.isFresh() || forced) {
+    if (storedArticles?.get()?.data) {
+      console.log('loading from store ...')
+      buildArticles(storedArticles.get().data)
+      document.body.querySelector('#newsfeed-wrap').scrollTo(0, 0)
+      document.body.classList.remove('loading')
+      return true
+    }
+    return false
+  }
+}
+
 /**
  * Gets the articles, defauts to 'home' section
  *
  * @param {string} [section='home']
  */
-export const fetchArticles = function (sSecton = 'home') {
+export const fetchArticles = function (sSection = 'home') {
   const loader = document.querySelector('#loader')
   loader.style.opacity = 1
-  if (!navigator.onLine) return
+  document.body.classList.add('loading')
+  let storedArticles
+  if (!navigator.onLine) {
+    storedArticles = getStoredArticles(sSection, true)
+  } else {
+    storedArticles = getStoredArticles(sSection)
+  }
 
-  const articles = new Promise(function (resolve, reject) {
-    document.body.classList.add('loading')
-    let data
-    fetch(API + '?section=' + sSecton)
-      .then(function (resp) {
-        document.body.classList.remove('loading')
-        data = resp.json()
-        if (resp.ok && resp.status === 200) {
+  if (!storedArticles) {
+    const fetchedArticles = new Promise(function (resolve, reject) {
+      let data
+      console.log('fetching fresh articles...')
+      fetch(API + '?section=' + sSection)
+        .then(function (resp) {
+          document.body.classList.remove('loading')
+          data = resp.json()
+          if (resp.ok && resp.status === 200) {
+            return data
+          } else {
+            throw resp
+          }
+        })
+        .then(function (data) {
+          if (data.top_stories.status !== 'OK') {
+            throw data
+          }
+          buildArticles(data)
+          const expiry = Date.now() + 120000 //  120000 = 2min
+          Satchel.setKey(sSection, { data, expiry }, true, 'svn-store')
+          document.body.querySelector('#newsfeed-wrap').scrollTo(0, 0)
           return data
-        } else {
-          throw resp
-        }
-      })
-      .then(function (data) {
-        if (data.top_stories.status !== 'OK') {
-          throw data
-        }
-        buildArticles(data)
-        document.body.querySelector('#newsfeed-wrap').scrollTo(0, 0)
-        return data
-      })
-      .catch(function (errs) {
-        loader.style.opacity = 0
-        const articlesEl = document.querySelector('#articles')
-        console.error(errs)
-        if (articlesEl) {
-          articlesEl.innerHTML = generateErrorsMarkup(errs)
-        }
-      })
-  })
-  return articles
+        })
+        .catch(function (errs) {
+          loader.style.opacity = 0
+          const articlesEl = document.querySelector('#articles')
+          console.error(errs)
+          if (articlesEl) {
+            articlesEl.innerHTML = generateErrorsMarkup(errs)
+          }
+        })
+    })
+    return fetchedArticles
+  }
 }
